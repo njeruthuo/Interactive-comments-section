69d9f7c33f23edf121e72285c3997e5240d7b068
njeruthuo <njeruthelearner@gmail.com>
2024-05-21 17:49:23 +0300

    #upto

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   // console.log(comments);
   return (
     <div className="min-h-screen bg-LightGray sm:flex justify-center">
-      <div className="sm:my-8 sm:w-1/2 border border-red-600 p-2">
+      <div className="sm:my-8 sm:w-1/2  p-2">
         {comments.map((comment) => {
           const { id } = comment;
           return <Notification key={id} {...comment} />;
